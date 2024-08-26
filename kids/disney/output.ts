d2da6c81789441804d06d6175ab3ad3fe8d69f75
Rajaghosh0007 <70210722+Rajaghosh0007@users.noreply.github.com>
2024-08-26 13:06:45 +0530

    Update output.ts

--- kids/disney/output.ts
+++ kids/disney/output.ts
@@ -2,5 +2,5 @@
 #EXT-X-VERSION:4
 #EXT-X-INDEPENDENT-SEGMENTS
 #EXT-X-STREAM-INF:BANDWIDTH=2141700,AVERAGE-BANDWIDTH=1832328,RESOLUTION=960x540,FRAME-RATE=25.000,CODECS="avc1.64001F,mp4a.40.2",AUDIO="audio_0"
-https://yuvraj.shodns.in/stalker_to_m3u/ozgsz/xyz.php?id=1097
+https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeebanglacinema1/default/zeebanglacinema1.m3u8
 
